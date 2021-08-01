19230a57b1b1337ad8a8e6e6f58681be03eaf91d
Nick King <nrking0@gmail.com>
2021-08-01 14:53:14 -0500

    Create basic mentor component

---
+++ src/Components/MentorTile/MentorTile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./MentorTile.css";
+import PropTypes from "prop-types";
+
+const MentorTile = (props) => {
+
+    return (
+        <div className="tile">
+            <img src={props.data.profile} />
+            <div className="info">
+                <p>
+                    {props.data.description}
+                </p>
+                <h1>{props.data.firstName + " " + props.data.lastName}</h1>
+            </div>
+        </div>
+    );
+};
+
+MentorTile.propTypes = {
+    data: PropTypes.object
+};
+
+export default MentorTile;
--- src/Pages/Mentors/Mentors.jsx
+++ src/Pages/Mentors/Mentors.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Mentors.css";
 import useMentors from "../../utils/useMentors";
+import MentorTile from "../../Components/MentorTile/MentorTile.jsx";
 
 const Mentors = () => {
-    
     const mentors = useMentors();
-    
-    return(
+
+    return (
         <div className="mentors-main">
-            <p>{JSON.stringify(mentors)}</p>
+            <h1>Meet our Mentors</h1>
+            <div className="tiles">
+                {mentors.map((mentor) => (
+                    <div key={mentor.firstname}  className="mentor-tile">
+                        <MentorTile data={mentor} />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
