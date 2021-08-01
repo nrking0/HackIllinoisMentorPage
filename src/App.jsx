74251d503f6d5b70cf976bdd938cec14c945e2d8
Nick King <nrking0@gmail.com>
2021-08-01 15:09:20 -0500

    Add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+/* eslint-disable no-undef */
 import React from "react";
 import "./App.css";
 import Mentors from "./Pages/Mentors/Mentors.jsx";
 import Navbar from "./Components/Navbar/Navbar.jsx";
+import Home from "./Pages/Home/Home.jsx";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <Mentors />
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router basename={process.env.PUBLIC_URL}>
+                <Navbar />
+                <Switch>
+                    <div className="main">
+                        <Route exact path="/" component={Home} />
+                        <Route path="/mentors" component={Mentors} />
+                    </div>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
