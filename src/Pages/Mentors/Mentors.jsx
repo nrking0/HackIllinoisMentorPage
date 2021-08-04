684fd39a5b3aa94a98c97950ce61aa0e8cf85401
Nick King <nrking0@gmail.com>
2021-08-04 15:01:44 -0500

    Clean up mentor page and add loading animation

--- src/Pages/Mentors/Mentors.jsx
+++ src/Pages/Mentors/Mentors.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Mentors.css";
 import useMentors from "../../utils/useMentors";
 import MentorTile from "../../Components/MentorTile/MentorTile.jsx";
+import ReactLoading from "react-loading";
 
 const Mentors = () => {
     const mentors = useMentors();
@@ -9,13 +10,26 @@ const Mentors = () => {
     return (
         <div className="mentors-main">
             <h1>Meet our Mentors</h1>
-            <div className="tiles">
-                {mentors.map((mentor) => (
-                    <div key={mentor.firstname}  className="mentor-tile">
-                        <MentorTile data={mentor} />
-                    </div>
-                ))}
-            </div>
+            {mentors.length === 0 ? (
+                <div className="loading">
+                    <ReactLoading
+                        type={"bubbles"}
+                        color={"#c07326"}
+                        height={"40%"}
+                        width={"40%"}
+                        className="loading-animation"
+                    />
+                    <h1>Loading Data...</h1>
+                </div>
+            ) : (
+                <div className="tiles">
+                    {mentors.map((mentor) => (
+                        <div key={mentor.firstname} className="mentor-tile">
+                            <MentorTile data={mentor} />
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
